e4882d50252c2467aebe9594cb51d63b8d792e4b
James Keen <james.keen.jr@gmail.com>
2022-06-26 22:47:52 -0500

    Demo->Clarity

--- source/Player.java
+++ source/Player.java
@@ -25,9 +25,9 @@ public class Player
 	}
 
 	public void attack(Monster monster) { // This method manages monster encounters
-		Demo.clearScreen();
+		Clarity.clearScreen();
 		this.isFighting = true;
-		Demo.topLine(3, "Pain");
+		Clarity.topLine(3, "Pain");
 		System.out.printf("%nA wild %s appears!%n", monster.getName());
 		while (this.isFighting) {
 			Scanner input = new Scanner(System.in);
@@ -46,15 +46,15 @@ public class Player
 	public void melee(Monster monster) { // This method handles the effects of choosing "attack" during a monster encounter.
 		int damage = r.nextInt(7) + 7;
 		monster.takeDamage(damage);
-		Demo.clearScreen();
-		Demo.topLine(3, "Pain");
+		Clarity.clearScreen();
+		Clarity.topLine(3, "Pain");
 		
 		System.out.printf("%nYou attack the %s for %d damage%n",monster.getName(), damage);
 		if (monster.getHealth() <= 0) {
 			this.honor += monster.getHonor();
 			System.out.printf("%n\033[3mQapla'\033[0m! You have defeated the %s.%n%n", monster.getName() );
 			this.isFighting = false;
-			Demo.pressEnter();
+			Clarity.pressEnter();
 		}
 		else {
 			takeDamage( monster.getStrength() );
@@ -63,13 +63,13 @@ public class Player
 	}
 	
 	public void flee() { // This method determines whether you successfully flee or not. 4/10 chance. Honor- loss.
-		Demo.clearScreen();
-		Demo.topLine(3, "Pain");
+		Clarity.clearScreen();
+		Clarity.topLine(3, "Pain");
 		int success = r.nextInt(10);
 		if (success > 5) {
 			this.isFighting = false;
 			System.out.println("\nYou run away from the creature, hoping it won't follow. It doesn't, but\nyou feel like a bit of a coward.\n");
-			Demo.pressEnter();
+			Clarity.pressEnter();
 		}
 		else 
 			System.out.println("\nYou try to run away but the creature skirts around you blocking your\nexit.\n");
